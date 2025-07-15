99654815461ea179541182cc2d4ef0614902ebd5
dev <dev@homelab.net>
2025-07-15 00:26:43 +0000

    update packge.json, components

--- projects/angular-dockview/src/lib/angular-dockview/dockview-container/dockview-container.component.ts
+++ projects/angular-dockview/src/lib/angular-dockview/dockview-container/dockview-container.component.ts
@@ -1,15 +1,91 @@
-import { Component, OnInit } from '@angular/core';
+// projects/angular-dockview/src/lib/dockview-container/dockview-container.component.ts
+
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import {
+  createSplitview,
+  SplitviewApi,
+  IView,
+  SerializedSplitview,
+  SplitviewComponentOptions,
+  SplitviewPanel, // abstract at compile time, but concrete at runtime
+} from 'dockview-core';
 
 @Component({
-  selector: 'lib-dockview-container',
-  templateUrl: './dockview-container.component.html',
-  styleUrls: ['./dockview-container.component.css']
+  selector: 'adv-dockview-container',
+  template: `<div #container class="dockview-container"></div>`,
+  styles: [
+    `
+      :host,
+      .dockview-container {
+        display: block;
+        width: 100%;
+        height: 100%;
+      }
+    `,
+  ],
 })
-export class DockviewContainerComponent implements OnInit {
+export class DockviewContainerComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('container', { static: true })
+  containerRef!: ElementRef<HTMLElement>;
 
-  constructor() { }
+  /**
+   * Optional overrides for SplitviewComponentOptions
+   * (disableAutoResizing, className, etc.).
+   * We’ll merge these into a full SplitviewComponentOptions below.
+   */
+  @Input() options?: Partial<
+    Omit<SplitviewComponentOptions, 'createComponent'>
+  >;
 
-  ngOnInit(): void {
+  /** Optional layout JSON to restore */
+  @Input() initialLayout?: SerializedSplitview;
+
+  @Output() didAddView = new EventEmitter<IView>();
+  @Output() didRemoveView = new EventEmitter<IView>();
+  @Output() layoutChange = new EventEmitter<void>();
+  @Output() layoutFromJSON = new EventEmitter<void>();
+
+  private api!: SplitviewApi;
+
+  ngAfterViewInit(): void {
+    // 1) Build a “full” SplitviewComponentOptions object, merging user overrides
+    //    and supplying a default createComponent factory. We cast to any at the end
+    //    so TS will stop complaining about the abstract class or missing members.
+    const fullOpts = {
+      // default panel factory: we cast SplitviewPanel to any to bypass `abstract`
+      createComponent: (params: any) =>
+        new (SplitviewPanel as any)(params.id, params.component),
+
+      // merge any other SplitviewOptions the user passed in
+      ...(this.options as any),
+    } as SplitviewComponentOptions;
+
+    // 2) Call the two-argument createSplitview(...)
+    this.api = createSplitview(this.containerRef.nativeElement, fullOpts);
+
+    // 3) Restore layout if provided
+    if (this.initialLayout) {
+      this.api.fromJSON(this.initialLayout);
+      this.layoutFromJSON.emit();
+    }
+
+    // 4) Wire up the SplitviewApi events exactly as declared in component.api.d.ts
+    this.api.onDidAddView((e) => this.didAddView.emit(e));
+    this.api.onDidRemoveView((e) => this.didRemoveView.emit(e));
+    this.api.onDidLayoutChange(() => this.layoutChange.emit());
+    this.api.onDidLayoutFromJSON(() => this.layoutFromJSON.emit());
   }
 
+  ngOnDestroy(): void {
+    this.api.dispose();
+  }
 }
--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,10 +1,44 @@
+// demo/src/app/app.component.ts
+
 import { Component } from '@angular/core';
+import { SplitviewPanel, CreateComponentOptions } from 'dockview-core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <adv-dockview-container
+      [options]="{ className: 'my-splitview' }"
+      [createComponent]="panelFactory"
+    ></adv-dockview-container>
+  `,
 })
 export class AppComponent {
-  title = 'demo';
+  /**
+   * The createComponent callback must:
+   *  - accept CreateComponentOptions { id, name }
+   *  - return a concrete SplitviewPanel with getComponent() implemented
+   */
+  panelFactory = (opts: CreateComponentOptions) => {
+    // Subclass the abstract SplitviewPanel at runtime
+    class AngularSplitviewPanel extends (SplitviewPanel as any) {
+      constructor() {
+        // Use opts.name (not opts.component)
+        super(opts.id, opts.name);
+      }
+
+      /**
+       * Implement the abstract method from BasePanelView:
+       * return the HTMLElement to be inserted for this panel.
+       */
+      getComponent(): HTMLElement {
+        const el = document.createElement('div');
+        el.style.width = '100%';
+        el.style.height = '100%';
+        // You could attach an Angular view here if needed.
+        return el;
+      }
+    }
+
+    return new AngularSplitviewPanel();
+  };
 }
