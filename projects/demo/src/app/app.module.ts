1e9140f9f62028f4db01c13214ac5e17900d5b10
dev1 <dev1@homelab.com>
2025-07-24 05:45:07 -0400

    Display a chart

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -2,19 +2,30 @@
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DateSalesPanelComponent } from './panels/date-sales-panel.component';
+import { CompanySalesPanelComponent } from './panels/company-sales-panel.component';
+import { ProductSalesPanelComponent } from './panels/product-sales-panel.component';
+import { PlotlyChartComponent } from './shared/plotly-chart/plotly-chart.component';
 
 // ← Import the wrapper library’s module
 import { AngularDockviewModule } from 'angular-dockview';
 
 @NgModule({
-  declarations: [AppComponent, DateSalesPanelComponent],
+  declarations: [
+    AppComponent,
+    DateSalesPanelComponent,
+    CompanySalesPanelComponent,
+    ProductSalesPanelComponent,
+    PlotlyChartComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularDockviewModule, // ← add here
+    AngularDockviewModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- projects/demo/src/app/panels/date-sales-panel.component.ts
+++ projects/demo/src/app/panels/date-sales-panel.component.ts
@@ -1,7 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService, SalesRecord } from '../services/data.services';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-date-sales-panel',
   templateUrl: './date-sales-panel.component.html',
 })
-export class DateSalesPanelComponent {}
+export class DateSalesPanelComponent implements OnInit {
+  plotData: any[] = [];
+  plotLayout: any = {};
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.dataService
+      .getSalesData()
+      .pipe(
+        map((records) => {
+          const aggregated = records.reduce((acc, curr) => {
+            acc[curr.date] =
+              (acc[curr.date] || 0) +
+              curr.red +
+              curr.yellow +
+              curr.white +
+              curr.blue;
+            return acc;
+          }, {} as { [key: string]: number });
+
+          return {
+            dates: Object.keys(aggregated),
+            totals: Object.values(aggregated),
+          };
+        })
+      )
+      .subscribe((data) => {
+        this.plotData = [
+          {
+            x: data.dates,
+            y: data.totals,
+            type: 'bar',
+            marker: { color: '#4c78a8' },
+          },
+        ];
+
+        this.plotLayout = {
+          title: 'Sales by Date',
+          xaxis: { title: 'Date' },
+          yaxis: { title: 'Number of Sales' },
+          margin: { t: 40, l: 50, r: 30, b: 60 },
+        };
+      });
+  }
+}
